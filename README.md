11cfee91889fd38d4eccbab75e7056b3eb438ed8
Mikko Kemppainen <mikko.kemppainen@qvik.fi>
2017-07-13 15:35:38 +0300

    deployment changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # flaskapp
-first exercise
+
+Deployment: 
+ssh into your Ubuntu VM and give the following commands:
+export IP=<external ip address of VM>
+git clone https://github.com/riikkaschroderus/flaskapp
+./flaskapp/deploy.sh
+
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python-pip
-pip install flask
-git clone https://github.com/riikkaschroderus/flaskapp
+sudo apt-get -y upgrade
+sudo apt-get install -y python-pip nginx
+sudo cp flaskapp/riikanflask /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/riikanflask /etc/nginx/sites-enabled/riikanflask
+pip install flask gunicorn
+export FLASK_APP=app.py
+cd flaskapp
+gunicorn -w 4 app:app
+
