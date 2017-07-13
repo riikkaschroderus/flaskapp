3c53ca46ae8ed891cfcb11e6043ce77defe5947d
Mikko Kemppainen <mikko.kemppainen@qvik.fi>
2017-07-13 17:09:44 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y python-pip nginx
 export IP=$(curl ipinfo.io/ip)
-sudo echo "server { listen 80; server_name ${IP}; location / { proxy_pass http://127.0.0.1:8000; proxy_set_header Host \$host; proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for; } }" > flaskapp/riikanflask
+sudo echo "server { listen 80; server_name ${IP}; location / { proxy_pass http://127.0.0.1:8000; proxy_set_header Host \$host; proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for; } }" > ~/flaskapp/riikanflask
 sudo cp flaskapp/riikanflask /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/riikanflask /etc/nginx/sites-enabled/riikanflask
 sudo systemctl reload nginx
 pip install flask gunicorn
 export FLASK_APP=app.py
-cd flaskapp
+cd ~/flaskapp
 gunicorn -w 4 app:app
