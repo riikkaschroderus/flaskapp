c876d546b9c48fbbb8b538a925e339036576c70b
Mikko Kemppainen <mikko.kemppainen@qvik.fi>
2017-07-13 16:09:51 +0300

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y python-pip nginx
+echo "server { listen 80; server_name ${IP}; location / { proxy_pass http://127.0.0.1:8000; proxy_set_header Host \$host; proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for; } }" > flaskapp/riikanflask
 sudo cp flaskapp/riikanflask /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/riikanflask /etc/nginx/sites-enabled/riikanflask
 sudo systemctl reload nginx
